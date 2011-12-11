38ce540484ea074f4418054aa50da56353cc011e
JangJunHyeok <jwjcorea@naver.com>
2011-12-11 21:48:04 +0900

    modified the input of the cal and package name

---
+++ TouchCalculator/src/com/androidjava/app/icalculator/DBManager.java
@@ -0,0 +1,27 @@
+package com.androidjava.app.icalculator;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBManager extends SQLiteOpenHelper {
+
+		public DBManager(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub		
+		arg0.execSQL("Create table userList (name text, tel text, mute integer, noreceive integer, sync integer);");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		arg0.execSQL("drop table userList;");
+	}
+}
---
+++ TouchCalculator/src/com/androidjava/app/icalculator/KeypadButtonCategory.java
@@ -0,0 +1,11 @@
+package com.androidjava.app.icalculator;
+
+public enum KeypadButtonCategory {
+	MEMORYBUFFER
+	, NUMBER
+	, OPERATOR
+	, DUMMY
+	, CLEAR
+	, RESULT
+	, OTHER
+}
